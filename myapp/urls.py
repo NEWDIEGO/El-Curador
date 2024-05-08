b356c4deb46c3ce0e9c2a5bf714499cedc889763
NEWDIEGO <di.medina@duocuc.cl>
2024-05-08 00:07:50 -0400

    cambios que revisar

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login, name='login'),
-    path('', views.login, name='home'),  # Esto manejará la ruta raíz
+    path('', views.hello, name='home'),  # Esto manejará la ruta raíz
 ]
--- mysite/settings.py
+++ mysite/settings.py
@@ -51,13 +51,10 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'mysite.urls'
 
-import os
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS':[os.path.join(BASE_DIR, 'myapp/templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
