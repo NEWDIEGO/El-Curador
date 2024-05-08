255475fbd5bd4c0c18918e0a8b2b254b7b254f6a
NEWDIEGO <di.medina@duocuc.cl>
2024-05-08 08:47:20 -0400

    Pagina web con exito

--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect
+from django.urls import path
+from django.shortcuts import render
 
+# es una vista
 
-# Create your views here.
+def login(request):
+    # Tu lógica de login aquí
+    return render(request, 'login.html')
 
 def hello(request): 
     return HttpResponse("<h1>Hello World</h1>")
