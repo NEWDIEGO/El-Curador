3c0b856cf795f27ea64d6be9c0efe30f2fb903c9
NEWDIEGO <di.medina@duocuc.cl>
2024-05-13 22:48:18 -0400

    new pass

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from . import views
+from django.contrib.auth import views as auth_views
 """
 urlpatterns = [
     path('login/', views.login, name='login'),
@@ -11,4 +12,8 @@ urlpatterns = [
     path('', include('django.contrib.auth.urls')),  # Incluye las URLs de autenticación
     path('', views.login_view, name='home'),
     path('register/', views.register, name='register'),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name='NewPass.html'), name='password_reset'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
 ]
