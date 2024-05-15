7709dc01d8b8ba6f0bf4aadb878eb4dc8a9dc52b
Kelho01 <ke.espinosa@duocuc.cl>
2024-05-15 13:13:47 -0400

    Segundo Commit
    Reorganizando y creando la datatable para lista de espera

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
-
+from myapp.views import vista_medico
+from myapp.views import Vista_EspecialistaPerfil
+from myapp.views import Vista_EspecialistaLista
 urlpatterns = [
     path('', views.hello),
     path('about/', views.about),
+    path('Especialista/', vista_medico),
+    path('Especialista/Perfil/', Vista_EspecialistaPerfil),
+    path('Especialista/Lista_de_espera/', Vista_EspecialistaLista),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,56 @@
 from django.http import HttpResponse
-
+from django.template import Template, Context
 # Create your views here.
 def hello(request): 
     return HttpResponse("<h1>Hello World</h1>")
 
 def about(request):
     return HttpResponse('About')
+
+def vista_medico(request):
+    # abrir el contenido del archivo HTML medico.html
+    plantilla_ext = open("myapp\Templates\Especialista.html")
+
+    # cargar el contenido del archivo HTML
+    template = Template(plantilla_ext.read())
+    
+    # cerrar el contenido del archivo HTML medico.html
+    plantilla_ext.close()
+    
+    #crear contexto
+    contexto = Context()
+
+    #renderizar documento
+    documento = template.render(contexto)
+    return HttpResponse(documento)
+
+def Vista_EspecialistaPerfil(request):
+    
+    plantilla_ext = open("myapp\Templates\EspecialistaPerfil.html")
+
+    
+    template = Template(plantilla_ext.read())
+    
+   
+    plantilla_ext.close()
+    
+   
+    contexto = Context()
+
+    
+    documento = template.render(contexto)
+    return HttpResponse(documento)
+
+def Vista_EspecialistaLista(request):
+    plantilla_ext = open("myapp\Templates\EspecialistaLista.html")
+
+    
+    template = Template(plantilla_ext.read())
+    
+    plantilla_ext.close()
+
+    contexto = Context()
+
+  
+    documento = template.render(contexto)
+    return HttpResponse(documento)
